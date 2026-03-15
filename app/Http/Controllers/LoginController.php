60534667de883312feb9babeecaf408360f65fa1
heiko <heiko.nevoigt@googlemail.com>
2026-03-15 12:51:03 +0100

    login route added

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle an authentication attempt.
+     */
+    public function login(Request $request): \Illuminate\Http\JsonResponse
+    {
+        var_dump($request->all());
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        $request->session()->regenerate();
+
+        return response()->json([
+            'message' => 'Login successful',
+            'user' => Auth::user()
+        ]);
+    }
+
+    public function logout(Request $request): \Illuminate\Http\JsonResponse
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return response()->json([
+            'message' => 'Logged out'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\HorseController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use function Pest\Laravel\json;
+
+Route::post('/login', [LoginController::class, 'login']);
+Route::get('/logout', [LoginController::class, 'logout']);
 
 Route::get('/', function (Request $request) {
     return 'something';
